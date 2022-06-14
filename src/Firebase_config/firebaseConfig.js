0f960542736a2999864a02953cc542e204dfea27
Ashana <60913775+ashana1647@users.noreply.github.com>
2022-06-15 00:42:31 +0530

    Add files via upload

---
+++ src/Firebase_config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCR7QOdD46I-ZkdEPXk4LLDBhFUBa3fNrw",
+    authDomain: "mygitapp-6bbb8.firebaseapp.com",
+    projectId: "mygitapp-6bbb8",
+    storageBucket: "mygitapp-6bbb8.appspot.com",
+    messagingSenderId: "23059977991",
+    appId: "1:23059977991:web:50a9c20522503edeef17a2",
+    measurementId: "G-RKT6RKX84B"
+  };
